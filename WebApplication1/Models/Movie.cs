c78812342d21dacb9f6b8cf49b1730a260ef5b54
aver803bath5 <aver803bath261@gmail.com>
2021-01-09 11:59:58 +0800

    Add Movies to the database.

--- WebApplication1/Models/Movie.cs
+++ WebApplication1/Models/Movie.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,10 @@ namespace Vidly.Models
     public class Movie
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public DateTime DateAdded { get; set; }
+        public int NumberInStock { get; set; }
     }
 }
