5a7a441bb4b6157f2a5052e98800b67c6cb02a01
aver803bath5 <aver803bath261@gmail.com>
2021-01-09 11:40:24 +0800

    Show customer birthday  in the customer details page.

--- WebApplication1/Controllers/CustomersController.cs
+++ WebApplication1/Controllers/CustomersController.cs
@@ -34,7 +34,7 @@ namespace Vidly.Controllers
         [Route("Customers/Details/{id:int}")]
         public ActionResult Details(int id)
         {
-            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
             {
