ba737ee6ec3a907a2d78c9c156abbb92d9aa8750
aver803bath5 <aver803bath261@gmail.com>
2021-01-11 14:34:08 +0800

    Add validation for customer form.

--- WebApplication1/Controllers/CustomersController.cs
+++ WebApplication1/Controllers/CustomersController.cs
@@ -49,6 +49,7 @@ namespace Vidly.Controllers
             var membershipTypes = _context.MembershipTypes.ToList();
             var viewModel = new CustomerFormViewModel
             {
+                Customer = new Customer(),
                 MembershipTypes = membershipTypes
             };
 
@@ -56,6 +57,7 @@ namespace Vidly.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(Customer customer)
         {
             if (!ModelState.IsValid)
