83bb20d62bd90d8bc528d61e55729ec92addfe7d
aver803bath5 <aver803bath261@gmail.com>
2021-01-12 20:19:09 +0800

    Install Automapping. Add Movie Dto.

---
+++ WebApplication1/App_Start/MappingProfile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using Vidly.Dtos;
+using Vidly.Models;
+
+namespace Vidly.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            Mapper.CreateMap<CustomerDto, Customer>();
+            Mapper.CreateMap<Customer, CustomerDto>();
+
+            Mapper.CreateMap<Movie, MovieDto>();
+            Mapper.CreateMap<MovieDto, Movie>();
+        }
+    }
+}
---
+++ WebApplication1/Dtos/MovieDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.Dtos
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime? ReleaseDate { get; set; }
+
+        [Range(0, 20)]
+        public int NumberInStock { get; set; }
+
+        public byte GenreId { get; set; }
+    }
+}
