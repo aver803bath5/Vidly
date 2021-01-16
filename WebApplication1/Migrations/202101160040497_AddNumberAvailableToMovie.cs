602d451c0ebdb44aa8d2ecca218c79272fb4730e
aver803bath5 <aver803bath261@gmail.com>
2021-01-16 09:01:25 +0800

    Add NumberAvailable property to Movie model
    * Set the type of NumberInStock to byte to match the type of NumberAvailable.
    * Change the range of NumberInStock to maintain at least one movie in the stock.
    * Set all NumberAvailable value to be the value of NumberInStock to initialize NumberAvailable field.

---
+++ WebApplication1/Migrations/202101160040497_AddNumberAvailableToMovie.cs
@@ -0,0 +1,21 @@
+namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddNumberAvailableToMovie : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Movies", "NumberAvailable", c => c.Byte(nullable: false));
+            AlterColumn("dbo.Movies", "NumberInStock", c => c.Byte(nullable: false));
+            Sql("UPDATE Movies SET NumberAvailable = NumberInStock");
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Movies", "NumberInStock", c => c.Int(nullable: false));
+            DropColumn("dbo.Movies", "NumberAvailable");
+        }
+    }
+}
--- WebApplication1/Models/Movie.cs
+++ WebApplication1/Models/Movie.cs
@@ -9,14 +9,22 @@ namespace Vidly.Models
     public class Movie
     {
         public int Id { get; set; }
+
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+
         public DateTime ReleaseDate { get; set; }
+
         public DateTime DateAdded { get; set; }
-        [Range(0, 20)]
-        public int NumberInStock { get; set; }
+
+        [Range(1, 20)]
+        public byte NumberInStock { get; set; }
+
+        public byte NumberAvailable { get; set; }
+
         public Genre Genre { get; set; }
+
         public byte GenreId { get; set; }
     }
 }
