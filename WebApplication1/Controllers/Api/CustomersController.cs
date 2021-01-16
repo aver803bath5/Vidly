9c35e4f6f4394c21c0241f657bf1c9211f47774d
aver803bath5 <aver803bath261@gmail.com>
2021-01-16 11:37:17 +0800

    Filter query result with query string.

--- WebApplication1/Controllers/Api/CustomersController.cs
+++ WebApplication1/Controllers/Api/CustomersController.cs
@@ -21,12 +21,21 @@ namespace Vidly.Controllers.Api
         }
 
         // GET /api/customers
-        public IEnumerable<CustomerDto> GetCustomers()
+        public IHttpActionResult GetCustomers(string query = null)
         {
-            return _context.Customers
-                .Include(c => c.MembershipType)
+            var customerQuery = _context.Customers
+                .Include(c => c.MembershipType);
+
+            if (!String.IsNullOrWhiteSpace(query))
+            {
+                customerQuery = customerQuery.Where(c => c.Name.Contains(query));
+            }
+                
+            var customerDtos = customerQuery
                 .ToList()
                 .Select(Mapper.Map<Customer, CustomerDto>);
+
+            return Ok(customerDtos);
         }
 
         // GET /api/customers/1
